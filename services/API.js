681f3f2520b6b772f9a99c5cb312bce53e61b470
Nikita Tselovalnikov <sosnovka@hotmail.com>
2023-07-24 20:36:53 +0000

    CM-002: Final App

--- services/API.js
+++ services/API.js
@@ -1,9 +1,10 @@
 const API = {
+    // url: "https://firtman.github.io/coffeemasters/api/menu.json",
     url: "/data/menu.json",
     fetchMenu: async () => {
-        const result = await fetch(API.url)
-        return await result.json()
+        const result = await fetch(API.url);
+        return await result.json();
     }
 }
 
-export default API
+export default API;
--- services/Router.js
+++ services/Router.js
@@ -3,51 +3,57 @@ const Router = {
         document.querySelectorAll("a.navlink").forEach(a => {
             a.addEventListener("click", event => {
                 event.preventDefault();
-                console.log("link clicked");
-                const url = event.target.getAttribute("href");
-                Router.go(url); 
+                const href = event.target.getAttribute("href");
+                Router.go(href);
             });
-        })
-        // Event Handler for url changes
-        window.addEventListener("popstate", event => {
+        });  
+        // It listen for history changes
+        window.addEventListener('popstate',  event => {
             Router.go(event.state.route, false);
-        })
-        // Check the initial url
+        });
+        // Process initial URL   
         Router.go(location.pathname);
-    },
+    },    
     go: (route, addToHistory=true) => {
-        console.log(`going to ${route} `);
-
         if (addToHistory) {
             history.pushState({ route }, '', route);
         }
         let pageElement = null;
         switch (route) {
             case "/":
-                pageElement = document.createElement("h1");
-                pageElement.textContent = "Menu";
+                pageElement = document.createElement("menu-page");
                 break;
             case "/order":
-                pageElement = document.createElement("h1");
-                pageElement.textContent = "Your Order";
+                pageElement = document.createElement("order-page");
                 break;
             default:
-                if (route.startsWith("/product-")) {
-                    pageElement = document.createElement("h1");
-                    pageElement.textContent = "Details";
-                    const paramId = route.substring(route.lastIndexOf("-")+1);
-                    pageElement.dataset.id = paramId;
+                if (route.startsWith("/product-")) {                
+                    pageElement = document.createElement("details-page");
+                    pageElement.dataset.productId = route.substring(route.lastIndexOf("-")+1);
                 }
-
+                break;   
         }
         if (pageElement) {
-            const cache = document.querySelector("main");
-            cache.innerHTML = "";
-            cache.appendChild(pageElement);
-            window.scrollX = 0;
-            window.scrollY = 0;
+            // get current page element            
+            let currentPage = document.querySelector("main").firstElementChild; 
+            if (currentPage) {
+                let fadeOut = currentPage.animate([
+                    {opacity: 1}, {opacity: 0}
+                ],{ duration: 200});
+                fadeOut.addEventListener("finish", () => {
+                    currentPage.remove();
+                    document.querySelector("main").appendChild(pageElement);
+                    let fadeIn = pageElement.animate([
+                        {opacity: 0}, {opacity: 1}
+                    ],{ duration: 200});
+                });
+            } else {
+                document.querySelector("main").appendChild(pageElement);
+            }
+
         }
 
+        window.scrollX = 0;
     }
 }
 
